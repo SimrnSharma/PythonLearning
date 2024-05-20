b8ea7eaf09a25cd8d4d7c30aef99f000e3ec43fd
SimrnSharma <simransharma0399@gmail.com>
2024-05-20 17:01:13 +0530

    With user imput

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -15,6 +15,22 @@ def group_anagrams(words):
 def sort_word(word):
         return ''.join(sorted(word))
 
-words = ["listen", "silent", "eat", "abc", "cab", "enlist", "tea", "ate", "mode", "demo"]
-grouped_anagrams = group_anagrams(words)
-print(grouped_anagrams)
+# words = ["listen", "silent", "eat", "abc", "cab", "enlist", "tea", "ate", "mode", "demo"]
+# grouped_anagrams = group_anagrams(words)
+# print(grouped_anagrams)
+
+def main():
+    print("Enter words separated by spaces:")
+    user_input = input()  
+    words = user_input.split()  
+    grouped_anagrams = group_anagrams(words)
+    print("Grouped anagrams:", grouped_anagrams)
+    wordList = list()
+    num = int(input("Enter number of words you'll like to add: "))
+    for i in range(0, num):
+        str = input(f"Enter word {i+1}: ")
+        wordList.append(str)
+    anagramList = group_anagrams(wordList)
+    print("Grouped anagrams:", anagramList)
+
+main()
